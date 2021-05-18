939c34e2de740aab4001849a4a9c1da3f018898b
周超 <zhouchao@mininglamp.com>
2021-05-18 23:28:51 +0800

    注释掉配置在发一版

--- data-ethics-gateway/docker-build-gateway-dockrhub.sh
+++ data-ethics-gateway/docker-build-gateway-dockrhub.sh
@@ -1,4 +1,4 @@
 mvn clean package -DskipTests
 docker build -t data-ethics-gateway .
-docker tag data-ethics-gateway zhouchaodocker/data-ethics-gateway:v1.0
-docker push zhouchaodocker/data-ethics-gateway:v1.0
+docker tag data-ethics-gateway zhouchaodocker/data-ethics-gateway:v1.3
+docker push zhouchaodocker/data-ethics-gateway:v1.3
