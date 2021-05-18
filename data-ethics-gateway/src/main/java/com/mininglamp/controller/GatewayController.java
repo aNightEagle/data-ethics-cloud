0905ebdf802152ad048a10584aaa740685719774
aNightAgle <351400268@qq.com>
2021-05-19 02:22:44 +0800

    ``还是刚才的探针测试 和阿里云修正

---
+++ data-ethics-gateway/src/main/java/com/mininglamp/controller/GatewayController.java
@@ -0,0 +1,31 @@
+package com.mininglamp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Description
+ * @Author xxxx
+ * @Version 1.0
+ **/
+
+@RestController
+@RequestMapping("/gateway")
+public class GatewayController {
+
+
+    @RequestMapping("test")
+    public String test(){
+
+        return "success";
+
+    }
+
+
+}
