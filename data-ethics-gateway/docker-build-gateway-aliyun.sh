7ced89497ff621dcd93b8a98062e829b7cd24163
aNightAgle <351400268@qq.com>
2021-05-19 01:41:24 +0800

    阿里云 gateway 打包镜像

---
+++ data-ethics-gateway/docker-build-gateway-aliyun.sh
@@ -0,0 +1,4 @@
+mvn clean package -DskipTests
+docker build -t data-ethics-gateway .
+docker tag data-ethics-gateway registry.cn-hangzhou.aliyuncs.com/itcrazy1998/test:v1.0
+docker push registry.cn-hangzhou.aliyuncs.com/itcrazy1998/data-ethics-gateway:v1.0
