d9f57704e83957f0797c559843c2718b4e01fe31
aNightEagle <35681185+aNightEagle@users.noreply.github.com>
2021-07-30 00:24:38 +0800

    Delete data-ethics-order-service directory

--- data-ethics-order-service/docker-build-order.sh
+++
@@ -1,4 +0,0 @@
-mvn clean package -DskipTests
-docker build -t cipher/data-ethics-order-service .
-docker tag cipher/data-ethics-order-service hub.docker.com/cipher/data-ethics-order-service:v1.0
-docker push hub.docker.com/cipher/data-ethics-order-service:v1.0
